b6766ccff7648aef186b2ad2309aeba814795861
Fabian Acosta <acosahf@hotmail.com>
2019-10-25 12:24:18 -0500

    got the grid for the game

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 console.log('hello people');
+
+//---------------------------cached element references-----------------------------------------------
+
+// const xPicEl = document.getElementById('x');
+
+// -------------------------------event listeners -------------------------------------------
+
+document.getElementById('cellBlock')
+    .addEventListener('click', cellClick);
+
+
+//------------------------------functions------------------------------------------------------
+
+
+
