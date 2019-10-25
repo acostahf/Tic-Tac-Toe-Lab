4ffc3839f25f5c398438f76c4ef98120179587c4
Fabian Acosta <acosahf@hotmail.com>
2019-10-25 17:06:33 -0500

    got the click event to work

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 console.log('hello people');
 
 // //---------------------------cached element references-----------------------------------------------
-
-let cellSelected = [];
+var board;
+const player1 = 'O';
+const player2 = 'X';
+// const winnginNums = [
+//     [0, 1, 2]
+//     [3, 4, 5]
+//     [6, 7, 8]
+//     [0, 3, 6]
+//     [1, 4, 7]
+//     [2, 5, 8]
+//     [0, 4, 8]
+//     [2, 4, 6]
+// ];
+// const cells = document.querySelectorAll('.cell')
 
 // // -------------------------------event listeners -------------------------------------------
 
-document.getElementById('td')
-    .addEventListener('click', addCellSelected);
-
+const boxes = document.querySelector('table').addEventListener('click', (evt) => {
+    console.log(evt.target);
+});
+// const boxes = document.querySelectorAll('td')
+// console.log(boxes)
 
 // //------------------------------functions------------------------------------------------------
 
-function addCellSelected() {
-    return cellN.push(cellSelected)
-};
+// function addCellSelected(x) {
+//     console.log(x.target.id)
+// }
