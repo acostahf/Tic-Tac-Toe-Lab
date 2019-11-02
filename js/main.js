80202a8eeaae3f27ef83790573c6019b11c3944c
Fabian Acosta <acosahf@hotmail.com>
2019-11-01 19:09:55 -0500

    restart js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,27 @@
 console.log('hello people');
 
-// //---------------------------cached element references-----------------------------------------------
-var board;
-const player1 = 'O';
-const player2 = 'X';
-// const winnginNums = [
-//     [0, 1, 2]
-//     [3, 4, 5]
-//     [6, 7, 8]
-//     [0, 3, 6]
-//     [1, 4, 7]
-//     [2, 5, 8]
-//     [0, 4, 8]
-//     [2, 4, 6]
-// ];
-// const cells = document.querySelectorAll('.cell')
+const winnginNums = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+const grid = () => Array.from(document.getElementsByClassName('cell'));
+const clickFn = (evt) => console.log(evt.target);
+const cNumId = (cEl) => Number.parseInt(cEl.id.replace('c', ''));
 
-// // -------------------------------event listeners -------------------------------------------
 
-const boxes = document.querySelector('table').addEventListener('click', (evt) => {
-    console.log(evt.target);
-});
-// const boxes = document.querySelectorAll('td')
-// console.log(boxes)
+const evtListener = () => grid().forEach(cEl => cEl.addEventListener('click', clickFn));
 
-// //------------------------------functions------------------------------------------------------
+console.log(cNumId);
 
-// function addCellSelected(x) {
-//     console.log(x.target.id)
-// }
+
+
+
+
+
+evtListener();
